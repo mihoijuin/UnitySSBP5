45360c0e0e1c953270f37f827e55639393e33b6b
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-07-03 14:11:44 +0900

    ClickUp #z643y 開始演出実装

---
+++ Assets/AppData/ScriptsSSBP5/OpPostEffect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class OpPostEffect : MonoBehaviour
+{
+ 	[SerializeField]
+    private Material opMaterial = null;
+
+    private void Awake() {
+        StartCoroutine(OP());
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+ 	{
+ 		Graphics.Blit (src, dest, opMaterial);
+ 	}
+
+    private IEnumerator SetMaterialFloat(float startValue, float endValue, float duration, Ease ease, float delay=0, bool initMode=false)
+    {
+        string[] properties = new string[] { "_VLWidth", "_HLWidth" };
+
+        foreach(string property in properties)
+        {
+            if(initMode) opMaterial.SetFloat(property, startValue);
+            yield return AppUtil.Wait(delay);
+            DOTween.To(
+                ()=> startValue,
+                x =>
+                {
+                    startValue = x;
+                    opMaterial.SetFloat(property, startValue);
+                },
+                endValue,
+                duration
+            ).SetEase(ease);
+        }
+    }
+
+    private IEnumerator OP() {
+        yield return AppUtil.Wait(1f);
+        yield return SetMaterialFloat(0.008f, 0.001f, 0.0005f, Ease.InExpo, 0.1f, true);
+
+        yield return AppUtil.Wait(0.8f);
+        yield return SetMaterialFloat(0.001f, 0.07f, 0.15f, Ease.InExpo);
+
+        yield return AppUtil.Wait(1.15f);
+        yield return SetMaterialFloat(0.07f, 0.8f, 0.8f, Ease.InQuart);
+    }
+}
---
+++ Assets/AppData/Shaders/SSBP5OP.shader
@@ -0,0 +1,35 @@
+﻿Shader "Unlit/SSBP5OP"
+{
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            float _HLWidth;
+            float _VLWidth;
+
+            fixed4 frag (v2f_img i) : COLOR
+            {
+                float vd = 0.28;
+                float hd = 0.35;
+                float minV = vd - _VLWidth;
+                float maxV = vd + _VLWidth;
+                float minH = hd - _HLWidth*0.8;
+                float maxH = hd + _HLWidth*0.8;
+                if((i.uv.x > minV && i.uv.x < maxV) || (i.uv.y > minH && i.uv.y < maxH))
+                {
+                    return fixed4(1,1,1,1);
+                } else
+                {
+                    return fixed4(0,0,0,1);
+                }
+            }
+            ENDCG
+        }
+    }
+}
